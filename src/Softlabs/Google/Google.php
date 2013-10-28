7ca19a30df9ad73a91964e148b4a6097fdfed2e2
Istvan Kadar <istvan.kadar@softlabs.co.uk>
2013-10-28 09:05:18 +0100

    Add test function

---
+++ src/Softlabs/Google/Google.php
@@ -0,0 +1,18 @@
+<?php namespace Softlabs\Google;
+
+use \Config as Config;
+
+class Google
+{
+
+    /**
+     * 
+     *
+     * 
+     */
+    public static function test() {
+        echo("test");
+    }
+
+
+}
